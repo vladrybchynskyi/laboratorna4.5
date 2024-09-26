f82f0773781b802043fd9eefb1773f6073bca505
vladrybchynskyi <vladyslav.rybchynskyi.ri.2024@lpnu.ua>
2024-09-26 11:20:32 +0300

    Update labor4_5.cpp

--- labor4_5.cpp
+++ labor4_5.cpp
@@ -1,33 +1,34 @@
 #include <iostream>
 #include <iomanip>
 #include <time.h>
+# include <cmath>
 using namespace std;
 int main()
 {
-  double x, y, R;
-  srand((unsigned)time(NULL));
-  for (int i = 0; i < 10; i++) {
-    cout << "x = "; cin >> x;
-    cout << "y = "; cin >> y;
-    cout << "R = "; cin >> R;
-    if ((y <= -sqrt(-x * x + 2 * x * R) + R && y >= 0 && x <= 0) ||
-      (y >= sqrt(-x * x + 2 * x * R) + R && y <= 0 && x >= 0))
-      cout << "Yes" << endl;
-    else
-      cout << "No" << endl;
-  }
-  cout << endl << fixed;
-  for (int i = 0; i < 10; i++)
-  {
-    x = rand() / RAND_MAX - R;
-    y = rand() / RAND_MAX - R;
-    if ((y <= -sqrt(-x * x + 2 * x * R) + R && y >= 0 && x <= 0) ||
-      (y >= sqrt(-x * x + 2 * x * R) + R && y <= 0 && x >= 0))
-      cout << setw(8) << setprecision(4) << x << " "
-      << setw(8) << setprecision(4) << y << " " << "yes" << endl;
-    else
-      cout << setw(8) << setprecision(4) << x << " "
-      << setw(8) << setprecision(4) << y << " " << "no" << endl;
-  }
-  return 0;
+    double x, y, R;
+    srand((unsigned)time(NULL));
+    for (int i = 0; i < 1; i++) {
+        cout << "x = "; cin >> x;
+        cout << "y = "; cin >> y;
+        cout << "R = "; cin >> R;
+        if ((y <= -sqrt(-x * x + 2 * x * R) + R && y >= 0 && x <= 0) ||
+            (y >= sqrt(-x * x + 2 * x * R) + R && y <= 0 && x >= 0))
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+    cout << endl << fixed;
+    for (int i = 0; i < 10; i++)
+    {
+        x =   6 * rand() / RAND_MAX - R;
+        y =   6 * rand() / RAND_MAX - R;
+        if ((y <= -sqrt(-x * x + 2 * x * R) + R && y >= 0 && x <= 0) ||
+            (y >= sqrt(-x * x + 2 * x * R) + R && y <= 0 && x >= 0))
+            cout << setw(8) << setprecision(4) << x << " "
+            << setw(8) << setprecision(4) << y << " " << "yes" << endl;
+        else
+            cout << setw(8) << setprecision(4) << x << " "
+            << setw(8) << setprecision(4) << y << " " << "no" << endl;
+    }
+    return 0;
 }
